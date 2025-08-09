88d7c4b308a3376d17bf64be3b6b7bba2165fcf8
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-08-09 21:38:59 +0530

    Cpp_Programs

--- fibonacci_series.cpp
+++ fibonacci_series.cpp
@@ -1,16 +1,26 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int main() {
+int main() 
+{
     int n, t1 = 0, t2 = 1, nextTerm;
-    cout << "Enter number of terms: ";
+
+    cout << "Enter Number of Terms : ";
+
     cin >> n;
 
-    for(int i = 1; i <= n; ++i) {
+
+    for(int i = 1; i <= n; ++i) 
+    {
         cout << t1 << " ";
+
         nextTerm = t1 + t2;
+
         t1 = t2;
+
         t2 = nextTerm;
     }
+    
     return 0;
 }
