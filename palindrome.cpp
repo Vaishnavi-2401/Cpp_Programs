5a6c3f8ecda6a04a9ba910e3d003b9a7db7ed71d
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-08-10 11:35:03 +0530

    Cpp_Programs

---
+++ palindrome.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+    int num, rev = 0, rem, temp;
+
+    cout << "Enter the Number : ";
+
+    cin >> num;
+
+    temp = num;
+
+    while(temp != 0) 
+    {
+        rem = temp % 10;
+
+        rev = rev * 10 + rem;
+
+        temp = temp / 10;
+    }
+
+    if(num == rev)
+    {
+        cout << "Palindrome";
+    }
+    else
+    {
+        cout << "Not a Palindrome";
+    }
+
+    return 0;
+}
