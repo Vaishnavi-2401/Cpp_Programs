79542682b2effc1e0ca57bd510feb137c7bd3ef9
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-08-16 12:16:12 +0530

    Cpp_Programs

--- anagram_strings.cpp
+++ anagram_strings.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-int main() {
+int main() 
+{
     string s1, s2;
-    cout << "Enter first string: ";
+
+    cout << "Enter First String : ";
     cin >> s1;
-    cout << "Enter second string: ";
+
+    cout << "Enter Second String : ";
     cin >> s2;
 
     sort(s1.begin(), s1.end());
     sort(s2.begin(), s2.end());
 
     if(s1 == s2)
+    {
         cout << "Anagram";
+    }
     else
+    {
         cout << "Not Anagram";
+    }
+    
     return 0;
 }
