1087b1bdc5877504b51aed513199c62ccb60818a
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-08-19 13:34:54 +0530

    Cpp_Programs

--- largest_element_in_array.cpp
+++ largest_element_in_array.cpp
@@ -1,21 +1,35 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int main() {
+int main() 
+{
     int n;
-    cout << "Enter number of elements: ";
+
+    cout << "Enter Number of Elements : ";
     cin >> n;
+
     int arr[n];
 
-    cout << "Enter elements: ";
+    cout << "Enter Elements : ";
+
     for(int i = 0; i < n; i++)
+    {
         cin >> arr[i];
 
-    int largest = arr[0];
-    for(int i = 1; i < n; i++)
-        if(arr[i] > largest)
-            largest = arr[i];
+    }
 
-    cout << "Largest element: " << largest;
+    int largest = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest Element : " << largest;
+    
     return 0;
 }
