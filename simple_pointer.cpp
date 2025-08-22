c7d330055c43d6770644d242ea3526fa3f585102
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-08-22 11:01:12 +0530

    Cpp_Programs

---
+++ simple_pointer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 42;
+    int *ptr = &num;
+
+    cout << "Value: " << num << endl;
+    cout << "Address: " << &num << endl;
+    cout << "Pointer points to: " << *ptr;
+    return 0;
+}
